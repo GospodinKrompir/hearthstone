49eadf7a4661db8c0bee04453b7e7b38476911b8
GospodinKrompir <alek988@gmail.com>
2019-04-09 14:58:55 +0200

    doradjen signin.js i izmene scss-a

--- src/components/signin.js
+++ src/components/signin.js
@@ -15,8 +15,20 @@ class SignIn extends Component {
                     <div>
                         <div className="signinForm">SIGN IN</div>
                     </div>
-                    <div>Forgot your password? *RESET* </div>
                     <div>You don't have account? *REGISTER*</div>
+                    <div>Forgot your password? *RESET* </div>
+                    <div>
+                        <label><span>E-mail:</span><input type="text" size="16" placeholder="E-mail"></input></label>
+                    </div>
+                    <div>
+                        <label><span>Password:</span><input type="password" size="16" placeholder="Password"></input></label>
+                    </div>
+                    <div>
+                        <label><span>Re-Password:</span><input type="password" size="16" placeholder="Re-Password"></input></label>
+                    </div>
+                    <div>
+                        <div className="resetPass">RESET</div>
+                    </div>
                 </form>
             </main>
         );
